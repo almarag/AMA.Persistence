d5608fc24e0406c22f821a43614a346f5b59fbf3
Alejandro Martinez <almarag@gmail.com>
2020-09-19 23:11:28 -0500

    Person Model updates

--- AMA.Persistence/Models/PersonModel.cs
+++ AMA.Persistence/Models/PersonModel.cs
@@ -1,9 +1,16 @@
 ﻿namespace AMA.Persistence.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class PersonModel : Model
     {
+        [Required]
         public string FirstName { get; set; }
+        
+        [Required]
         public string LastName { get; set; }
+        
+        [Required]
         public string PhoneNumber { get; set; }
     }
 }
